e7f8e6334315029dd3df0bc5b0308530b201ab14
mbettcher <mtonon.pmg@gmail.com>
2021-09-26 17:41:45 -0300

    Primeiro Commit Estrutura do Projeto Criado

---
+++ src/test/java/br/com/mtonon/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
